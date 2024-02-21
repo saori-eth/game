5fec3582961c970571be9a33fb71fc622a7a9a9a
saori-eth <D84da@proton.me>
2024-02-21 17:58:13 -0600

    move join button to middle

--- src/client/components/Join.tsx
+++ src/client/components/Join.tsx
@@ -23,7 +23,9 @@ export const Join = ({ setRoom }: any) => {
     }
     return (
         <>
-            <button onClick={handleClick}>Join</button>
+            <button onClick={handleClick} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              Join
+            </button>
         </>
     )
 }
