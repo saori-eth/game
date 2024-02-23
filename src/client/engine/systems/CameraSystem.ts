b92baf9bdf5a165f53a7168deeaed1aaf555ed1c
saori-eth <D84da@proton.me>
2024-02-23 02:45:54 -0600

    clean, add first person

--- src/client/engine/systems/CameraSystem.ts
+++ src/client/engine/systems/CameraSystem.ts
@@ -1,6 +1,11 @@
 import { EngineService } from '@/engine'
 import { PerspectiveCamera } from 'three'
 
+const MOUSE_SENSITIVITY = 0.005
+const ZOOM_SENSITIVITY = 0.01
+const MAX_ZOOM = 10
+const MIN_ZOOM = 0.01
+
 export class CameraSystem {
     private engine: EngineService
     public camera: PerspectiveCamera
@@ -20,47 +25,44 @@ export class CameraSystem {
 
     public update(): void {
         const player = this.engine.players.find(
-            (player) => player.id === this.engine.multiplayerSystem.id
+            (p) => p.id === this.engine.multiplayerSystem.id
         )
         if (!player || !player.mesh || !player.position) return
-        const controls = this.engine.controlSystem.controls
-        // Convert movementX and movementY to angle changes
-        const angleX = controls.movementX * 0.005 // Sensitivity adjustment
-        // Reverse the effect of movementY by subtracting it
-        const angleY = -controls.movementY * 0.005 // Sensitivity adjustment
 
-        // Update azimuthal and polar angles
+        const playerPos = player.position
+
+        const { movementX, movementY, deltaY } =
+            this.engine.controlSystem.controls
+        const angleX = movementX * MOUSE_SENSITIVITY,
+            angleY = -movementY * MOUSE_SENSITIVITY
+
         this.cameraAzimuthalAngle += angleX
         this.cameraPolarAngle = Math.max(
             0.1,
             Math.min(Math.PI / 2, this.cameraPolarAngle + angleY)
-        ) // Clamp the polar angle
+        )
 
-        // Calculate new camera position relative to the player's position
         const x =
-            player.position.x +
+            playerPos.x +
             this.cameraDistance *
                 Math.sin(this.cameraPolarAngle) *
                 Math.cos(this.cameraAzimuthalAngle)
         const y =
-            player.position.y +
-            this.cameraDistance * Math.cos(this.cameraPolarAngle)
+            playerPos.y + this.cameraDistance * Math.cos(this.cameraPolarAngle)
         const z =
-            player.position.z +
+            playerPos.z +
             this.cameraDistance *
                 Math.sin(this.cameraPolarAngle) *
                 Math.sin(this.cameraAzimuthalAngle)
 
         this.camera.position.set(x, y, z)
-        this.camera.lookAt(
-            player.position.x,
-            player.position.y,
-            player.position.z
-        )
+        this.camera.lookAt(playerPos.x, playerPos.y, playerPos.z)
 
-        // Update camera distance based on input
-        this.cameraDistance -= -controls.deltaY * 0.01 // Sensitivity adjustment
-        this.cameraDistance = Math.max(2, Math.min(20, this.cameraDistance)) // Clamp the distance
+        this.cameraDistance += deltaY * ZOOM_SENSITIVITY
+        this.cameraDistance = Math.max(
+            MIN_ZOOM,
+            Math.min(MAX_ZOOM, this.cameraDistance)
+        )
 
         this.engine.controlSystem.resetControls()
     }
