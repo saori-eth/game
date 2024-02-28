f65af3bb99213a5ab7aa1c7cc6de809818fae2b0
saori-eth <D84da@proton.me>
2024-02-27 23:03:14 -0600

    window resize

--- src/client/engine/systems/RenderSystem.ts
+++ src/client/engine/systems/RenderSystem.ts
@@ -9,6 +9,12 @@ export class RenderSystem {
         this.renderer = new WebGLRenderer({ canvas: engine.canvas })
         this.renderer.setSize(window.innerWidth, window.innerHeight)
         this.renderer.setPixelRatio(window.devicePixelRatio)
+        window.addEventListener('resize', () => {
+            this.renderer.setSize(window.innerWidth, window.innerHeight)
+            this.engine.cameraSystem.camera.aspect =
+                window.innerWidth / window.innerHeight
+            this.engine.cameraSystem.camera.updateProjectionMatrix()
+        })
     }
     update(delta: number) {
         this.renderer.render(this.engine.scene, this.engine.cameraSystem.camera)
